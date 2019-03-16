cc758c0b00dce49c53c6ff02a40ac1365ec78b3b
tjddbs0401 <48537546+tjddbs0401@users.noreply.github.com>
2019-03-16 16:40:33 +0900

    Update P_num.py

--- P_num.py
+++ P_num.py
@@ -1,9 +1,9 @@
-import custom_math 
-count = input("Input: ");
+import custom_math #custom_math.py에서 가져오기
+count = input("Input: "); #n을 입력 받음
 i = 2 
-while 1 : 
+while 1 : #참일 때 계속 수행
     if(custom_math.isPrime(i)) : 
-        count = count -1
+        count = count -1 
     if(count == 0): 
         break
     i = i+1
