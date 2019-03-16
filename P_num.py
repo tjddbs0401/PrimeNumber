b186b43e282a3153c03642f3118324473e74b20a
tjddbs0401 <48537546+tjddbs0401@users.noreply.github.com>
2019-03-16 16:12:13 +0900

    Create P_num.py

---
+++ P_num.py
@@ -0,0 +1,10 @@
+import custom_math
+count = input("Input: ");
+i = 2
+while 1 : 
+    if(custom_math.isPrime(i)) :
+        count = count -1
+    if(count == 0): 
+        break
+    i = i+1
+print("Number is =", i)
